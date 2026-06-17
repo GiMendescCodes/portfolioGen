80372e456a79d302b32b3e27f9552c4e6d34ab09
GimendescCodes <giovannamendessenai@gmail.com>
2026-06-17 19:48:42 -0300

    css pag principal

---
+++ script.js
@@ -0,0 +1,8 @@
+const projectsSwiper = new Swiper('.projects-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    pagination: {
+        el: '.projects-swiper .swiper-pagination',
+        clickable: true,
+    },
+});
